2c6fa6b06049b594f86b24d271b994336830f217
rock-hopper <rock-hopper@users.noreply.github.com>
2014-07-04 20:08:35 +0100

    license changes

--- src/JuceLibraryCode/modules/juce_audio_devices/native/juce_linux_JackAudio.cpp
+++ src/JuceLibraryCode/modules/juce_audio_devices/native/juce_linux_JackAudio.cpp
@@ -21,27 +21,13 @@
 
   ==============================================================================
 
-  This file contains code originally written by "jpo"
+  This file contains modifications originally written by "jpo"
   <http://www.juce.com/comment/296820#comment-296820>
 
-  All modifications to this file are part of Shuriken.
+  Additional modifications to this file by Andrew M Taylor <a.m.taylor303@gmail.com>, 2014
 
-  Copyright (C) 2014 Andrew M Taylor <a.m.taylor303@gmail.com>
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program. If not, see <https://www.gnu.org/licenses/>
-  or write to the Free Software Foundation, Inc., 51 Franklin Street,
-  Fifth Floor, Boston, MA  02110-1301, USA.
+  All modifications to the original file are released into the public domain.
+  Please read UNLICENSE for more details, or refer to <http://unlicense.org/>
 
 */
 
--- src/JuceLibraryCode/modules/juce_audio_devices/native/relay_libjack.cpp
+++ src/JuceLibraryCode/modules/juce_audio_devices/native/relay_libjack.cpp
@@ -1,25 +1,12 @@
 /*
-  This file is part of Shuriken Beat Slicer.
-
-  Copyright (C) 2014 Andrew M Taylor <a.m.taylor303@gmail.com>
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program. If not, see <https://www.gnu.org/licenses/>
-  or write to the Free Software Foundation, Inc., 51 Franklin Street,
-  Fifth Floor, Boston, MA  02110-1301, USA.
-
   This file contains code originally written by "jpo"
   <http://www.juce.com/comment/296820#comment-296820>
+
+  All modifications to this file by Andrew M Taylor <a.m.taylor303@gmail.com>, 2014
+
+  This is free and unencumbered software released into the public domain.
+  Please read UNLICENSE for more details, or refer to <http://unlicense.org/>
+
 */
 
 #include <dlfcn.h>
