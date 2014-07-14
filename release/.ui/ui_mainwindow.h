9c7986a4ba752c552556de03aaf9f7f6392bd465
rock-hopper <rock-hopper@users.noreply.github.com>
2014-07-14 21:44:22 +0100

    update .gitignore

--- release/.ui/ui_mainwindow.h
+++ release/.ui/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Mon Jul 14 12:01:31 2014
+** Created: Mon Jul 14 21:06:56 2014
 **      by: Qt User Interface Compiler version 4.6.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
